b78bce1fe9eb9e6649d08e86aa7d8a8fa2aadf00
Ricky Grosvenor-Pramanick <ronakpramanick@gmail.com>
2023-12-22 21:03:19 -0500

    Added Data Visualisation Components

--- CardSafe/card_safe.py
+++ CardSafe/card_safe.py
@@ -11,15 +11,36 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 data = pd.read_csv("creditcard.csv")
 
-# Exploratory Data Analysis
+# Exploratory Data Analysis - Data Visualisation
 print("Number of Fraudulent Transactions: " + str(data[data['Class'] == 1].shape[0]))
 print("Number of Legitimate Transactions: " + str(data[data['Class'] == 0].shape[0]))
 print("Proportion of Fraudulent Transactions: " + str(data[data['Class'] == 1].shape[0]/data.shape[0]))
 
 data_pi = data.copy()
-data_pi[" "] =np.where(data_pi['Class'] == 1, "Fraud", "Genuine")
+data_pi[" "] = np.where(data_pi['Class'] == 1, "Fraud", "Legitimate")
 
 # %matplotlib inline
-data_pi[" "].value_counts().plot(kind='pie')
+
+# Pie Chart depicting proportion of Fradulent to Legitimate Transactions
+# data_pi[" "].value_counts().plot(kind='pie')
 
 # data.describe()
+
+f, axes = plt.subplots(1, 2, figsize=(18, 4), sharex='all')
+
+amount_val = data['Amount'].values
+time_val = data['Time'].values
+
+sns.displot(
+    amount_val,
+    color="m",
+    kde_kws={"shade": True},
+    ax=axes[0]).set_title("Distribution of Transaction Amounts")
+
+sns.displot(
+    time_val,
+    color="m",
+    kde_kws={"shade": True},
+    ax=axes[1]).set_title("Distribution of Transaction Times")
+
+plt.show()
